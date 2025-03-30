dc9cfc8e4cf499a1c4f8929ec048c9acdef22fd7
Akila Fernando <144292336+Akila-Fernando@users.noreply.github.com>
2025-03-29 22:55:21 -0600

    Made Courses more "pretty"

--- my-lms-app/src/components/CourseCatalog.js
+++ my-lms-app/src/components/CourseCatalog.js
@@ -1,16 +1,19 @@
 import React from "react";
 import CourseItem from "./CourseItem";
 import courses from "../data/courses";
+import "./CourseCatalog.css";
 
 const CourseCatalog = ({ enroll }) => {
   return (
-    <div>
+    <div class name="course-catalog">
       <h2>Courses</h2>
+      <div className="course-grid">
       {courses.map(course => (
         <CourseItem key={course.id} course={course} enroll={enroll} />
       ))}
+      </div>
     </div>
   );
 };
 
-export default CourseCatalog; 
+export default CourseCatalog;
--- my-lms-app/src/components/CourseItem.js
+++ my-lms-app/src/components/CourseItem.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import courseimage from '../images/course1.jpg'
+import "./CourseItem.css";
 
 function CourseItem({course, enrollCourse}) {
     const [showDescription, setShowDesc] = useState(false);
 
     return (
         
-        <div className="course-item" onMouseEnter={() => setShowDesc(true)} onMouseLeave={() => setShowDesc(false)}
-            style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '10px', marginBottom: '10px'}}>
+        <div className="course-item" onMouseEnter={() => setShowDesc(true)} onMouseLeave={() => setShowDesc(false)}>
 
             <img src={courseimage} width={250} height={250}></img>
             <h3>Course Name: {course.name}</h3>
